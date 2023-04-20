db2cff29c5ba2680bc53d26b53dd3bc972b5078e
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2023-04-20 06:04:00 +0200

    Add list of valid users to use

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -4,13 +4,15 @@ const jwt = require("jsonwebtoken");
 
 const secret = "chatroom-secret";
 
+const VALID_USERS = ["Rayyan", "Chrome", "Edge", "Medo"];
+
 router.post("/login", (req, res, next) => {
   // Authenticate user and get user ID
   const userId = req.body.username;
   const password = req.body.password;
   console.log("req", userId, password);
 
-  if (userId === "Rayyan" && password === "123") {
+  if (VALID_USERS.includes(userId) && password === "123") {
     // Generate JWT token for user
     const token = jwt.sign({ userId }, secret, { algorithm: "HS256" });
 
